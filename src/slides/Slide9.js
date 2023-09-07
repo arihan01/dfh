40ad5742aa400d564ad56045f1fc36192ddba6d7
arihan01 <arihanawasthi01.com>
2023-09-07 20:11:51 +0530

    .

--- src/slides/Slide9.js
+++ src/slides/Slide9.js
@@ -22,8 +22,8 @@ export default function Slide9() {
       <svgs.s9Text3 className='w-full h-auto' />
     </motion.div> */}
 <motion.div className='left-align mb-10' {...animate.rightToLeft}>
-  <div className='font-bvs text-xl lg:text-3xl'>CANOPY WALK AND<br />INTERPRETATION CENTRE</div>
-  <div className='font-cf text-xs lg:text-base'>
+  <div className='font-bvs text-2xl lg:text-3xl'>CANOPY WALK AND<br />INTERPRETATION CENTRE</div>
+  <div className='font-cf text-sm lg:text-base'>
     <span style={{color: '#E59617'}}>Camera traps</span> in strategic locations,<br />
     designing a platform that allows access<br/>
     to <span style={{color: '#E59617'}}>200+ years old canopy</span> 40 ft above the<br />
@@ -32,16 +32,16 @@ export default function Slide9() {
   </div>
 </motion.div>
 <motion.div className='right-align mb-10 ml-2' {...animate.leftToRight}>
-  <div className='font-bvs text-right text-xl lg:text-3xl'>NATIVE FOREST<br/>PRODUCE</div>
-  <div className='font-cf text-right text-xs lg:text-base'>Western Ghats hosts over <span style={{color: '#E59617'}}>9000 species</span><br/>
+  <div className='font-bvs text-right text-2xl lg:text-3xl'>NATIVE FOREST<br/>PRODUCE</div>
+  <div className='font-cf text-right text-sm lg:text-base'>Western Ghats hosts over <span style={{color: '#E59617'}}>9000 species</span><br/>
     of plants. Our team at Dancing Frog<br/>
     is in the process of cataloguing the<br/>
     medicinal plants
   </div>
 </motion.div>
 <motion.div className='left-align mr-3' {...animate.rightToLeft}>
-  <div className='font-bvs text-xl lg:text-3xl'>EXPERIENCES</div>
-  <div className='font-cf text-xs lg:text-base'>Forest experiences or the viewing<br/>
+  <div className='font-bvs text-2xl lg:text-3xl'>EXPERIENCES</div>
+  <div className='font-cf text-sm lg:text-base'>Forest experiences or the viewing<br/>
     of forest scenes contributes to<br/>
     <span style={{color: '#E59617'}}>reducing stress, promoting positive</span><br/>
     <span style={{color: '#E59617'}}>feelings</span> resulting in general wellness.<br/>
