a24f99c9b35a93e1393c0b4fba0c82911d528730
arihan01 <arihanawasthi01.com>
2023-09-19 22:35:22 +0530

    .

--- src/slides/Slide9.js
+++ src/slides/Slide9.js
@@ -5,7 +5,7 @@ import * as animate from "../animations";
 
 export default function Slide9() {
   return (
-    <div className="flex flex-col items-center justify-center h-[100dvh] white-bg bg-common">
+    <div className="flex flex-col items-center justify-center h-full white-bg bg-common">
       {/* <motion.img src={require('./img/s9-top.png')} className='w-4/5 mb-10 mt-2' {...animate.fadeIn} /> */}
       {/* <motion.div className='w-4/5 mt-5'>
       <svgs.s9Top className='w-full h-auto' />
