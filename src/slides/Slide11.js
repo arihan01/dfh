75b9b9e559e042c238ff61889e4d4220681e504b
arihan01 <arihanawasthi01.com>
2023-12-15 16:30:44 +0530

    updates

--- src/slides/Slide11.js
+++ src/slides/Slide11.js
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import * as animate from "../animations";
-import { ReactComponent as S11Text1 } from "../img/s11-text-1.svg";
-import { ReactComponent as S11Text2 } from "../img/s11-text-2.svg";
-import { ReactComponent as S11Text3 } from "../img/s11-text-3.svg";
-import { ReactComponent as S11Text4 } from "../img/s11-text-4.svg";
+import { ReactComponent as S11Arrow } from "../img/s11-arrow.svg";
 
 export default function Slide11() {
-  const [imageStates, setImageStates] = useState([true, true, true, true]);
+  // const [imageStates, setImageStates] = useState([true, true, true, true]);
 
-  const toggleImage = (index) => {
-    const newImageStates = [...imageStates];
-    newImageStates[index] = !newImageStates[index];
-    setImageStates(newImageStates);
-  };
+  // const toggleImage = (index) => {
+  //   const newImageStates = [...imageStates];
+  //   newImageStates[index] = !newImageStates[index];
+  //   setImageStates(newImageStates);
+  // };
   return (
-    <div className="orange-bg bg-common flex h-full flex-col items-center justify-center">
-      <motion.div
+    <div className="white-bg bg-common flex h-full items-center justify-center">
+      
+      {/* <motion.div
         className="font-bvb text-4xl leading-[5vh] md:mt-16 md:text-5xl "
         {...animate.fadeIn}
       >
@@ -126,7 +124,99 @@ export default function Slide11() {
             <S11Text4 className="my-1 h-auto w-full" />
           </motion.div>
         )}
-      </motion.div>
+      </motion.div> */}
+
+      <div className="flex h-4/5 flex-col justify-center md:gap-10">
+        <motion.div {...animate.fadeIn}>
+          <p className="text-center font-bvb text-4xl md:text-5xl">
+            Collective
+          </p>
+          <p className="-mt-2 text-center font-bvr text-4xl text-[#A83D35] md:text-5xl">
+            Testimonies
+          </p>
+        </motion.div>
+
+        <motion.div {...animate.leftToRight}>
+          <div className="mx-auto mt-5 flex w-4/5 items-center justify-center gap-2">
+            <img
+              src={require("../img/s11-img-1.png")}
+              alt="s11 img 1"
+              className="h-2/5 w-2/5"
+            />
+            <div className="flex w-4/5 flex-col justify-center">
+              <p className="font-bvb text-xl md:text-3xl">Deepthi Tanikella</p>
+              <p className="font-cf text-xs">
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud
+              </p>
+              <div className="mt-2 flex items-center gap-2">
+                <S11Arrow className="h-4 w-4" />
+                <a
+                  href="https://www.youtube.com/watch?v=XbkA1gxE6Lk"
+                  className="rounded-full bg-[#A83D35] p-2 px-5 text-center font-cf text-xs text-white"
+                >
+                  Watch here
+                </a>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div {...animate.rightToLeft}>
+          <div className="mx-auto mt-5 flex w-4/5 items-center justify-center gap-2">
+            <div className="flex w-4/5 flex-col justify-center">
+              <p className="font-bvb text-xl md:text-3xl">Vinod Chandramouli</p>
+              <p className="font-cf text-xs">
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud
+              </p>
+              <div className="mt-2 flex items-center gap-2">
+                <S11Arrow className="h-4 w-4" />
+                <a
+                  href="https://www.youtube.com/watch?v=XbkA1gxE6Lk"
+                  className="rounded-full bg-[#A83D35] p-2 px-5 text-center font-cf text-xs text-white"
+                >
+                  Watch here
+                </a>
+              </div>
+            </div>
+            <img
+              src={require("../img/s11-img-2.png")}
+              alt="s11 img 1"
+              className="h-2/5 w-2/5"
+            />
+          </div>
+        </motion.div>
+
+        <motion.div {...animate.leftToRight}>
+          <div className="mx-auto mt-5 flex w-4/5 items-center justify-center gap-2">
+            <img
+              src={require("../img/s11-img-1.png")}
+              alt="s11 img 1"
+              className="h-2/5 w-2/5"
+            />
+            <div className="flex w-4/5 flex-col justify-center">
+              <p className="font-bvb text-xl md:text-3xl">Deepthi Tanikella</p>
+              <p className="font-cf text-xs">
+                lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud
+              </p>
+              <div className="mt-2 flex items-center gap-2">
+                <S11Arrow className="h-4 w-4" />
+                <a
+                  href="https://www.youtube.com/watch?v=XbkA1gxE6Lk"
+                  className="rounded-full bg-[#A83D35] p-2 px-5 text-center font-cf text-xs text-white"
+                >
+                  Watch here
+                </a>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 }
