fcd81b05263d58b84ba99a9b21db6d8029dcce16
arihan01 <arihanawasthi01.com>
2023-12-18 23:35:18 +0530

    new slide - 2

--- src/slides/Slide14.js
+++ src/slides/Slide14.js
@@ -12,7 +12,10 @@ import { ReactComponent as S14Logo } from "../img/s14-logo.svg";
 export default function Slide1() {
   return (
     <div className="white-bg-2 bg-common flex items-center justify-center">
-      <div className="flex h-[90vh] w-4/5 flex-col items-center justify-center">
+      <motion.div
+        className="flex h-[90vh] w-4/5 flex-col items-center justify-center"
+        {...animate.fadeIn}
+      >
         <div>
           <p className="w-full text-right font-bvb text-lg text-[#2E663F] md:text-xl">
             Ecology
@@ -21,14 +24,14 @@ export default function Slide1() {
             Explore <span className="text-[#E59617]">Mycelium</span>
           </p>
         </div>
-        <Logo className="mx-auto w-3/5 h-auto mt-5" />
-        <p className="font-cf text-xs mt-2">
+        <Logo className="mx-auto mt-5 h-auto w-3/5" />
+        <p className="mt-2 font-cf text-xs">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim
           veniam tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam
         </p>
-        <div className="flex items-center gap-10 mt-2">
+        <div className="mt-2 flex items-center gap-10">
           <a href="https://www.mycelium.today/">
             <S13W className="h-auto w-6" />
           </a>
@@ -42,45 +45,47 @@ export default function Slide1() {
             <S13Yt className="h-auto w-7" />
           </a>
         </div>
-        <p className="text-center font-bvb text-3xl md:text-5xl mt-5">
+        <p className="mt-5 text-center font-bvb text-3xl md:text-5xl">
           Dancing Frog
           <br />
           <span className="font-bvr text-4xl md:text-6xl">Habitat</span>
         </p>
-        <p className="font-cf text-xs mt-2">
+        <p className="mt-2 font-cf text-xs">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim
           veniam tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam
         </p>
-        <div className="flex gap-10 items-center mt-2">
-            <a href="https://www.mycelium.today/dancingfroghabitat">
-                <S13W className="w-6 h-auto"/>
-            </a>
-            <a href="https://www.instagram.com/dancingfroghabitat/">
-                <S13Ig className="w-6 h-auto"/>
-            </a>
-            <a href="https://www.youtube.com/playlist?list=PL5OpUZshh2nwKFOAtKzPEmrPGFLsCAldy">
-                <S13Yt className="w-7 h-auto"/>
-            </a>
+        <div className="mt-2 flex items-center gap-10">
+          <a href="https://www.mycelium.today/dancingfroghabitat">
+            <S13W className="h-auto w-6" />
+          </a>
+          <a href="https://www.instagram.com/dancingfroghabitat/">
+            <S13Ig className="h-auto w-6" />
+          </a>
+          <a href="https://www.youtube.com/playlist?list=PL5OpUZshh2nwKFOAtKzPEmrPGFLsCAldy">
+            <S13Yt className="h-auto w-7" />
+          </a>
         </div>
-        <p className="text-center font-bvb font-bold text-3xl md:text-5xl mt-5">
+        <p className="mt-5 text-center font-bvb text-3xl font-bold md:text-5xl">
           Connecting
           <br />
-          <span className="font-bvs text-4xl md:text-6xl font-light">the dots</span>
+          <span className="font-bvs text-4xl font-light md:text-6xl">
+            the dots
+          </span>
         </p>
-        <p className="font-cf text-xs mt-2">
+        <p className="mt-2 font-cf text-xs">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          veniam tempor incididunt ut labore et dolore magna aliqua. Ut enim a
           minim veniam
         </p>
-        <div className="flex gap-10 items-center mt-2">
-            <a>
-                <S14Logo className="w-5 h-auto"/>
-            </a>
+        <div className="mt-2 flex items-center gap-10">
+          <a href="https://www.mycelium.today/">
+            <S14Logo className="h-auto w-5" />
+          </a>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
