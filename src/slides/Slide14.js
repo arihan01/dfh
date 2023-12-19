6eb1745f3a985e8bc39d406afa77d4ff918ab539
arihan01 <arihanawasthi01.com>
2023-12-19 11:26:15 +0530

    icons update

--- src/slides/Slide14.js
+++ src/slides/Slide14.js
@@ -7,7 +7,8 @@ import { ReactComponent as S13Ig } from "../img/s13-ig.svg";
 import { ReactComponent as S13In } from "../img/s13-in.svg";
 import { ReactComponent as S13Yt } from "../img/s13-yt.svg";
 import { ReactComponent as S13W } from "../img/s13-w.svg";
-import { ReactComponent as S14Logo } from "../img/s14-logo.svg";
+import { ReactComponent as Substack } from "../img/substack-icon.svg";
+import { ReactComponent as CTD } from "../img/CTD.svg";
 
 export default function Slide1() {
   return (
@@ -24,12 +25,11 @@ export default function Slide1() {
             Explore <span className="text-[#E59617]">Mycelium</span>
           </p>
         </div>
-        <Logo className="mx-auto mt-5 h-auto w-3/5" />
+        <Logo className="mx-auto mt-5 h-auto w-3/5 md:mt-10" />
         <p className="mt-2 font-cf text-xs">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam
+          veniam tempor incididunt ut labore et dolore magna aliqua.
         </p>
         <div className="mt-2 flex items-center gap-10">
           <a href="https://www.mycelium.today/">
@@ -53,8 +53,7 @@ export default function Slide1() {
         <p className="mt-2 font-cf text-xs">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam
+          veniam tempor incididunt ut labore et dolore magna aliqua.
         </p>
         <div className="mt-2 flex items-center gap-10">
           <a href="https://www.mycelium.today/dancingfroghabitat">
@@ -67,22 +66,15 @@ export default function Slide1() {
             <S13Yt className="h-auto w-7" />
           </a>
         </div>
-        <p className="mt-5 text-center font-bvb text-3xl font-bold md:text-5xl">
-          Connecting
-          <br />
-          <span className="font-bvs text-4xl font-light md:text-6xl">
-            the dots
-          </span>
-        </p>
+        <CTD className="mx-auto mt-5 h-auto w-3/5 md:w-4/5" />
         <p className="mt-2 font-cf text-xs">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam tempor incididunt ut labore et dolore magna aliqua. Ut enim a
-          minim veniam
+          veniam tempor incididunt ut labore et dolore magna aliqua.
         </p>
         <div className="mt-2 flex items-center gap-10">
-          <a href="https://www.mycelium.today/">
-            <S14Logo className="h-auto w-5" />
+          <a href="https://coexist.substack.com/?utm_source=substack&utm_medium=web&utm_campaign=substack_profile">
+            <Substack className="h-auto w-5" />
           </a>
         </div>
       </motion.div>
